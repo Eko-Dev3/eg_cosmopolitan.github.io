01bdd54a692939077f78a6aa9b82edefe013b2a1
EKO <nsueekojuanantonio@gmail.com>
2023-03-10 08:59:10 +0000

    Actualizacion hoy viernes

---
+++ git.cmd.cmd
@@ -0,0 +1,11 @@
+git remote add origin
+git branch -M main
+git push -u origin main
+
+ git add .
+git commit -am "Actualizacion hoy viernes"
+git push origin main
+
+git push --set-upstream origin master
+
+git push -u origin --all
